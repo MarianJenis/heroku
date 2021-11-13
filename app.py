5b59321cd8466436c9151d0f54c06ae4ff24ce54
MarianJenis <73757276+MarianJenis@users.noreply.github.com>
2021-11-13 15:15:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,4 @@ nordic_animals = ["los", "sob", "tučňák", "vlk", "lední medvěd"]
 
 @app.route("/")
 def index():
-    return render_template("index.html", age=3)
-
-
-@app.route("/cool_animals_list")
-def cool_animals_list():
-    return render_template("cool_animals.html", animals=nordic_animals)
-
-
-@app.route("/memes")
-def memes():
-    return render_template("memes.html")
-
-if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+    return "<p>Ahoj robo</p>"
