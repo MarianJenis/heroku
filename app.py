a2b1a0388e88796e5577f9393d0867b4ce46d48d
MarianJenis <73757276+MarianJenis@users.noreply.github.com>
2021-11-13 15:06:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask
-import hashlib
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+nordic_animals = ["los", "sob", "tučňák", "vlk", "lední medvěd"]
+
+
 @app.route("/")
-@app.route("/name/<name>")
-def hello_there(name: str = "General Kenobi"):
-    return f"<p>Hello there, {name}!</p>"
+def index():
+    return render_template("index.html", age=3)
 
-@app.route("/ksi/<challenge>")
-def ksi_challenge(challenge: str):
-    # This function is here to allow you to get a trophy!
-    return hashlib.sha256(f"{challenge}-KSI".encode("utf8")).hexdigest()
 
+@app.route("/cool_animals_list")
+def cool_animals_list():
+    return render_template("cool_animals.html", animals=nordic_animals)
+
+
+@app.route("/memes")
+def memes():
+    return render_template("memes.html")
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
