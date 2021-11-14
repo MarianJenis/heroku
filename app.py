5478687c521dca72aa4021023a3907ea0e73d639
MarianJenis <73757276+MarianJenis@users.noreply.github.com>
2021-11-14 09:26:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello_there(name: str = "Robo"):
     return f"<p>Nazdar, {name}!</p>", 200
 
+@app.route("/ksi/<challenge>")
+def challange():
+    return f"<p>Challange passed</p>, 200
+
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
